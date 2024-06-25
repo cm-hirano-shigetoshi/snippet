c0459cb8dfec753de56b31fbd36badc31a445bec
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-06-26 01:22:07 +0900

    ディレクトリ内を巡る

--- main/snippet.sh
+++ main/snippet.sh
@@ -1,25 +1,30 @@
 readonly TOOL_DIR="$(dirname $(perl -MCwd=realpath -le 'print realpath shift' "$0"))"
-readonly SNIPPET_FILE="${HOME}/.local/share/zinit/plugins/cm-hirano-shigetoshi---snippet/snippet"
+readonly SNIPPET_DIR="${HOME}/.local/share/snippet"
 
 temp_dir=$(mktemp -dt "XXXXXXXX")
 mkdir -p "$temp_dir"
-grep -hn '^###' "$SNIPPET_FILE" | \
+
+grep -Sr '^' "$SNIPPET_DIR" | \
+    sed -e '/:###/!s%^[^:]\+:%%' -e '/:###/s%^.*/\([^/]\+\)\.md:###%### \1% ' \
+    > $temp_dir/snippet
+
+grep -hn '^###' "$temp_dir/snippet" | \
     grep -o '^\d\+' | \
     cat -n \
     > $temp_dir/index
 
-selected=$(sed -n 's/### \(.*\)/\1/p' "$SNIPPET_FILE" | \
+selected=$(sed -n 's/### \(.*\)/\1/p' "$temp_dir/snippet" | \
     fzf \
     --reverse \
     --bind 'enter:become:echo enter; echo {n};' \
     --bind 'alt-enter:become:echo alt-enter; echo {n};' \
-    --preview "${TOOL_DIR}/preview.sh \"$SNIPPET_FILE\" ${temp_dir} \$(echo {n})" \
+    --preview "${TOOL_DIR}/preview.sh \"$temp_dir/snippet\" ${temp_dir} \$(echo {n})" \
     --preview-window "wrap:down:50%" \
 )
 if [[ -n "${selected}" ]]; then
     header="$(head -1 <<< "${selected}")"
     num="$(sed 1d <<< "${selected}" | tr '' '\n')"
-    content="$("${TOOL_DIR}/get_code.sh" "$SNIPPET_FILE" ${temp_dir} $num)"
+    content="$("${TOOL_DIR}/get_code.sh" "$temp_dir/snippet" ${temp_dir} $num)"
     if [[ "${header}" = "alt-enter" ]]; then
         echo "${content}" > "$temp_dir/selected"
         $EDITOR "$temp_dir/selected"
@@ -31,5 +36,5 @@ if [[ -n "${selected}" ]]; then
     fi
 fi
 
-selected=$("${TOOL_DIR}/get_code.sh" "$SNIPPET_FILE" ${temp_dir} $selected_num)
+selected=$("${TOOL_DIR}/get_code.sh" "$temp_dir/snippet" ${temp_dir} $selected_num)
 
