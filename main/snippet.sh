ad877a8260d74bcbdb63701b286dae6b9d4a6ba8
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-06-27 00:02:23 +0900

    スクリプト編集機能

--- main/snippet.sh
+++ main/snippet.sh
@@ -1,12 +1,16 @@
 readonly TOOL_DIR="$(dirname $(perl -MCwd=realpath -le 'print realpath shift' "$0"))"
 readonly SNIPPET_DIR="${HOME}/.local/share/snippet"
 
-temp_dir=$(mktemp -dt "XXXXXXXX")
-mkdir -p "$temp_dir"
+temp_dir=$(mktemp -dt "snippet-XXXXXXXX")
 
 grep -Sr '^' "$SNIPPET_DIR" | \
     sed -e '/:###/!s%^[^:]\+:%%' -e '/:###/s%^.*/\([^/]\+\)\.md:###%### \1% ' \
     > $temp_dir/snippet
+
+grep -Srn '^###' "$SNIPPET_DIR" | \
+    sed 's/:###.*$//' | \
+    sed 's/:/ /' \
+    > $temp_dir/path-line
 
 grep -hn '^###' "$temp_dir/snippet" | \
     grep -o '^\d\+' | \
@@ -18,6 +22,7 @@ selected=$(sed -n 's/### \(.*\)/\1/p' "$temp_dir/snippet" | \
     --reverse \
     --bind 'enter:become:echo enter; echo {n};' \
     --bind 'alt-enter:become:echo alt-enter; echo {n};' \
+    --bind 'alt-e:become:echo alt-e; echo {n};' \
     --preview "${TOOL_DIR}/preview.sh \"$temp_dir/snippet\" ${temp_dir} \$(echo {n})" \
     --preview-window "wrap:down:70%" \
 )
@@ -31,6 +36,10 @@ if [[ -n "${selected}" ]]; then
         if [[ -s "$temp_dir/selected" ]]; then
             cat "$temp_dir/selected" | perl -pe 'chomp if eof' | pbcopy
         fi
+    elif [[ "${header}" = "alt-e" ]]; then
+        read path line < <(sed -n "$((${num}+1))p" "$temp_dir/path-line")
+        echo "$EDITOR $path +$line" >> /tmp/aaa
+        $EDITOR $path +$line
     else
         echo "${content}" | perl -pe 'chomp if eof' | pbcopy
     fi
