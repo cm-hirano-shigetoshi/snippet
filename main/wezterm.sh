30ce2692106bfa3ea85be3b3ace632672fc935a0
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-10-10 00:25:38 +0900

    denoをPATHに追加

--- main/wezterm.sh
+++ main/wezterm.sh
@@ -7,6 +7,7 @@ trap "$WEZTERM_EXECUTABLE_DIR/wezterm cli activate-pane --pane-id $PREV_PANE_ID"
 # WezTermで直接プロセス起動した場合zshrcが読まれない
 PATH="$PATH:/opt/homebrew/bin"
 PATH="$PATH:$HOME/.local/share/mise/installs/node/latest/bin"
+PATH="$PATH:$HOME/.local/share/mise/installs/deno/latest/bin"
 
 readonly TOOL_DIR="$(dirname $(perl -MCwd=realpath -le 'print realpath shift' "$0"))"
 bash "$TOOL_DIR/snippet.sh"
